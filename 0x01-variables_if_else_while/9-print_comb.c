ae414725b2dc85bb66a209414d0b8efe56f36114
bilalabdelkadir <bilalbinabdelkadir@gmail.com>
2022-03-11 12:40:40 -0800

    fixed some result issue

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
 /**
- * main - main block
- * Description: prints all single digit numbers of base 10
- * starting from 0, followed by a new line.
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int c = 0;
+	int p = 48, q = 48, y = 48, x = 49;
 
-	while (c < 10)
+	while (p < 58)
 	{
-		putchar(48 + c);
-		if (c != 9)
+		while (q < 58)
 		{
-			putchar(',');
-			putchar(' ');
+			while (y < 58)
+			{
+				while (x < 58)
+				{
+					putchar(p);
+					putchar(q);
+					putchar(' ');
+					putchar(y);
+					putchar(x);
+					if (!(p == 57 &&
+						q == 56 &&
+						y == 57 &&
+						x == 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					x++;
+				}
+				y++;
+				x = 48;
+			}
+			q++;
+			q = p;
+			x = q + 1;
 		}
-		c++;
+		p++;
+		q = 48;
+		y = p;
+		x = q+ 1;
 	}
 	putchar('\n');
 	return (0);
